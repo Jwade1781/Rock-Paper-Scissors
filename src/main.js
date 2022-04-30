336cc49b93f0b7a97f80507f140d8e4d8b8a631c
Jwade1781 <jwade5219@gmail.com>
2022-04-30 15:05:46 -0400

    General HTML index design implemented

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+
+function play(){
+    const playerLayer = document.getElementsByClassName("layer3")[0];
+    const playButton = document.getElementById("playButton");
+    playButton.style.visibility = "hidden";
+    playerLayer.style.visibility = "visible";
+}
